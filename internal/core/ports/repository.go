20909cbf8b3a491c89cf4b3b0e7db08cbc317473
sarthak0714 <sarthak.t1403@gmail.com>
2024-10-09 20:10:15 +0530

    switched to pg/neon, removed ctx

--- internal/core/ports/repository.go
+++ internal/core/ports/repository.go
@@ -1,19 +1,17 @@
 package ports
 
 import (
-	"context"
-
 	"github.com/sarthak0714/backend-task-sc/internal/core/domain"
 )
 
 type TradeRepository interface {
-	AddTrade(ctx context.Context, trade *domain.Trade) error
-	UpdateTrade(ctx context.Context, id int64, trade *domain.Trade) error
-	RemoveTrade(ctx context.Context, id int64) error
-	FetchTrades(ctx context.Context, userID string) ([]*domain.Trade, error)
+	AddTrade(trade *domain.Trade) error
+	UpdateTrade(id int64, trade *domain.Trade) error
+	RemoveTrade(id int64) error
+	FetchTrades(userID string) ([]*domain.Trade, error)
 }
 
 type PortfolioRepository interface {
-	UpsertPortfolio(ctx context.Context, portfolio *domain.Portfolio) error
-	FetchPortfolio(ctx context.Context, userID string) ([]*domain.Portfolio, error)
+	UpsertPortfolio(portfolio *domain.Portfolio) error
+	FetchPortfolio(userID string) ([]*domain.Portfolio, error)
 }
